8fa2aa131a4156f678fccecefd76f20ad3188f17
Arnaud BRACCHETTI <abracchetti@gmail.com>
2016-07-31 17:36:23 +0200

    Nettoyage du service JIRA-users pour plus de lisibilité

--- app/Services/JIRA-users/JIRA-users.service.ts
+++ app/Services/JIRA-users/JIRA-users.service.ts
@@ -1,18 +1,15 @@
 import { Injectable, Inject } from '@angular/core';
 import {Http, Headers, Response} from '@angular/http';
-import 'rxjs/rx';
+import 'rxjs/add/operator/mergemap';
+import 'rxjs/add/observable/zip';
 import 'rxjs/add/operator/do';
 import {Observable} from 'rxjs/Observable';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
-/*export class JIRAUser2 {
-     key  : string;
-     name : string;
-     mail : string;
-     team : string;
-     login : string;
-}*/
 
+/* interfaces correspondantes aux structures de données retournées
+ * par les services REST de JIRA.
+ */
 interface REST_JIRAProject {
     id : number;
     key : string;
@@ -31,6 +28,13 @@ interface REST_JIRARole {
     actors : REST_JIRAUser[];
 }
 
+/* Classes exportées qui constitue la structure des données gérée 
+ * par le service.
+ * 
+ * - JIRAUser
+ *     - JIRAProject
+ *          - JIRARole
+ */
 export class JIRAProject {
     id : number;
     key : string;
@@ -74,31 +78,20 @@ export class JIRARole {
 }
 
 
+
+/* Classe du services fournissant les données utilisateurs
+ * 
+ */
 @Injectable()
 export class JIRAUsersService{
     
-    // TODO : nettoyer la classe
-   /* mockUser: JIRAUser[] = 
-    [{name:"user1", mail:"mail1", team : "team1"},
-     {name:"user2", mail:"mail2", team : "team2"},
-     {name:"user3", mail:"mail3", team : "team1"},
-     {name:"user4", mail:"mail4", team : "team1"},
-     {name:"user5", mail:"mail5", team : "team2"},
-     {name:"user6", mail:"mail6", team : "team1"},
-     {name:"user7", mail:"mail7", team : "team3"},
-     {name:"user8", mail:"mail8", team : "team1"},
-     {name:"user9", mail:"mail9", team : "team3"},
-     {name:"user10", mail:"mail10", team : "team3"},
-     {name:"user11", mail:"mail11", team : "team2"},
-    
-    ];*/
+   
     
     private JIRAhttp : Http;
     private _users: BehaviorSubject<JIRAUser[]> = new BehaviorSubject<JIRAUser[]>([]);
     public users: Observable<JIRAUser[]> = this._users.asObservable();
     
-    //private _users : Map<string,JIRAUser> = new Map<string,JIRAUser>();
-    //private _projects: JIRAProject[] = [];
+  
     
     constructor(@Inject(Http) aHttp: Http) {
         
@@ -106,48 +99,11 @@ export class JIRAUsersService{
         }
     
     
-    /* Instancie un tableau de JIRAUser à partir des données retournées
-     * par le webservice de récupération des comptes JIRA.
-     * Seules les informations de base sont initialisées.
-     */ 
-    /*private fillJIRAUser(data):JIRAUser[]
-    {
-        let users: JIRAUser2[] = [];
-                 
-        data.forEach((item) => {
-                                        let target: JIRAUser2 = new JIRAUser2();
-                                        target.key = item.key;
-                                        target.name = item.displayName;
-                                        target.mail = item.emailAddress;
-                                        
-                                        users.push(target);
-                                    })
-          
-         console.log(users);                                                     
-        return users;
-                                        
-    }*/
-     
-    
-    /*
-     * Complete l'initialisation du tableau de JIRAUser avec des informations
-     * demendant des requetes suplémentaires
-     */
-    /*private completeUser()
-    {
-        console.log("completeUser CALL");
-    }   */
-    
-   /* private _addProject(id:number, key:string, name:string)
-    {
-        let prj: JIRAProject = new JIRAProject();
-        prj.id = id;
-        prj.key = key;
-        prj.name = name;
-        
-        this._projects.push(prj);
-    }*/
-    
+ 
+ /* Cette methode construit progressivement la structure de données 
+  * gérée par le service. A chaque étape de la construction la nouvelle
+  * structure de données et emise aux observeurs.
+  */   
     private _buildUserDataStructure(role : REST_JIRARole, project : REST_JIRAProject)
     {
         
@@ -176,11 +132,14 @@ export class JIRAUsersService{
         }
         
         this._users.next(this._users.getValue())
-       // console.count();
-       // console.dirxml(this._users);
+       
     }
   
-    public getUsers(): Observable<JIRAUser[]>//JIRAUser[]>
+    /* Cette methode interoge JRIA sur les information des utilisateurs
+     * en réponse aux requetes effectuées, la structure de données est complètée
+     * par un appel à la methode privé _buildUserDataStructure()
+     */
+    public getUsers(): Observable<JIRAUser[]>
     {
         let header:Headers = new Headers();
         //header.append("Authorization", 'Basic c3lzYWRtaW46UG9sdXgkMTQ=');
@@ -193,22 +152,18 @@ export class JIRAUsersService{
         let userUrl : string    = 'http://163.62.118.20/rest/api/2/user/search?username=%&maxResults=500'; 
         let projectUrl : string = 'http://163.62.118.20/rest/api/2/project'; 
         
-       
-       /* let userReq: Observable<JIRAUser[]> = this.JIRAhttp.get(userUrl,{headers:header, withCredentials:true})
-             .map((response) => this.fillJIRAUser(response.json()));*/
-             
+                   
         this.JIRAhttp.get(projectUrl,{headers:header, withCredentials:true})
             .flatMap(response => Observable.from(response.json()))
-            //.do(data=> console.dir(data))
-            //.do(data => this._addProject(data.id, data.key, data.name))
+            
           
             .flatMap((project : REST_JIRAProject) => Observable.zip(this.JIRAhttp.get("http://163.62.118.20/rest/api/2/project/"+project.key+"/role",{headers:header, withCredentials:true})
                                                 .flatMap(response => Observable.from(Object.values(response.json()))),    
                                             projectRole=>[projectRole,project]))
-            //.do(data => console.log("inner : " + JSON.stringify(data)))
+            
             .flatMap(projectRole => Observable.zip(this.JIRAhttp.get(projectRole[0],{headers:header, withCredentials:true}),
                                             roleDetail => [roleDetail.json(), projectRole[1]]))
-            .do((data) => {/*console.dir(data[0]);*/ this._buildUserDataStructure(data[0], data[1])})
+            .do((data) => {this._buildUserDataStructure(data[0], data[1])})
             .subscribe();
                 
                 
